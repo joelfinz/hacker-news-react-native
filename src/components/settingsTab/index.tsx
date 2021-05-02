ab5e7aadbb49d49bd3439b445bb9ed5b2878803c
joelfinz <joelfintan@gmail.com>
2021-05-02 14:10:26 +0530

    Added bottom tab navigation
    * Added Settings

---
+++ src/components/settingsTab/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { View } from "react-native"
+import { Text } from "react-native-paper"
+
+const SettingsTab = () => {
+  return (
+    <View>
+      <Text>
+      Settings Tab
+      </Text>
+    </View>
+  )
+}
+
+export default SettingsTab;
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,41 +1,53 @@
 import React from "react"
 import { View } from "react-native"
-import { Button, Text } from "react-native-paper"
-import {
-  Tabs,
-  TabScreen,
-  useTabIndex,
-  useTabNavigation,
-} from 'react-native-paper-tabs';
+import { Button, Text, BottomNavigation } from "react-native-paper";
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BestStoriesTab from "../../components/bestStoriesTab";
 import NewStoriesTab from "../../components/newStoriesTab";
 import TopStoriesTab from "../../components/topStoriesTab";
+import SettingsTab from "../../components/settingsTab";
 
-
+const Tab = createMaterialBottomTabNavigator();
 
 const Home = ({ navigation }: any) => {
-
+  
   return (
-    <View>
-      <Tabs defaultIndex={0}>
-        <TabScreen label="New">
-          <View>
-            <NewStoriesTab />
-          </View>
-        </TabScreen>
-        <TabScreen label="Top">
-          <View>
-            <TopStoriesTab />
-          </View>
-        </TabScreen>
-        <TabScreen label="Best">
-          <View>
-            <BestStoriesTab />
-          </View>
-        </TabScreen>
-      </Tabs>
-      <Button onPress={() => navigation.navigate('About')}>About page</Button>
-    </View>
+    <Tab.Navigator
+      backBehavior={"initialRoute"}
+      initialRouteName={"new_stories"}
+
+    >
+      <Tab.Screen
+        name="new_stories"
+        component={NewStoriesTab}
+        options={{
+          title: "New", tabBarIcon: "alert-circle"
+        }}
+      />
+      <Tab.Screen
+        name="top_stories"
+        component={TopStoriesTab}
+        options={{
+          title: "Top", tabBarIcon: "arrow-up"
+        }}
+      />
+      <Tab.Screen
+        name="best_stories"
+        component={BestStoriesTab}
+        options={{
+          title: "Best", tabBarIcon: "thumb-up"
+        }}
+      />
+      <Tab.Screen
+        name="app_settings"
+        component={SettingsTab}
+        options={{
+          title: "Settings", tabBarIcon: "cog"
+        }}
+      />
+      </Tab.Navigator>
   )
 }
 
